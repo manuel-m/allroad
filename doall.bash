1eec1c8fa5b24de82f45264c7915e95fda1650f7
dr <discoride.dev@gmail.com>
2015-05-25 20:49:37 +0200

    doall.bash => build allroad_template.bz2

--- doall.bash
+++ doall.bash
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+
 gulp
+rm -rf allroad_template 2>/dev/null
+mv dist allroad_template
+rm -rf allroad_template/app
+tar cvfj allroad_template.bz2 allroad_template
+rm -rf allroad_template
